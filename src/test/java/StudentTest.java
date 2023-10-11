2468fbf5680d0a447e4fa8cb912123dfceae81a0
ZainabHY <zhalyousif@gmail.com>
2023-10-11 21:17:49 +0300

    Adding Student class and StudentTest

---
+++ src/test/java/StudentTest.java
@@ -0,0 +1,41 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class StudentTest {
+    SchoolData schoolData;
+    Student student1;
+    Student student2;
+
+    @BeforeEach
+    void setUp()
+    {
+        schoolData = new SchoolData();
+        student1 = new Student("Dana Sami", "Dammam", "dana.sami@gmail.com");
+        student2 = new Student("Salem Fahad", "Khobar", "salem.fahad@gmail.com");
+        schoolData.studentsList.add(student1);
+        schoolData.studentsList.add(student2);
+    }
+
+    @Test
+    public void testGenerateStudentId() {
+        String studentId1 = student1.getStudentId();
+        String studentId2 = student2.getStudentId();
+
+        Assertions.assertNotNull(studentId1);
+        Assertions.assertNotNull(studentId2);
+        Assertions.assertTrue(studentId1.startsWith("stu"));
+        Assertions.assertTrue(studentId2.startsWith("stu"));
+        Assertions.assertNotEquals(studentId1, studentId2);
+    }
+
+    @Test
+    public void testFindStudentById() {
+        Student foundStudent = student1.findStudentById(student1.getStudentId(), schoolData);
+
+        Assertions.assertNotNull(foundStudent);
+        Assertions.assertEquals(student1, foundStudent);
+    }
+}
